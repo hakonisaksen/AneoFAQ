22dbf58fa06c44755bb451ff616f07c9bfe37667
Håkon Lund-Isaksen <hakon.isaksen@aneo.com>
2023-06-12 16:04:20 +0200

    GTP integration with file handler

--- FAQ.py
+++ FAQ.py
@@ -5,3 +5,23 @@ import os
 
 os.environ["OPEN_API_KEY"] = "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"
 
+def createVectorIndex(path):
+    max_input = 4096
+    tokens = 256
+    chunk_size = 600
+    max_chunk_overlap = 20
+
+    prompt_helper = PromptHelper(max_input, tokens, max_chunk_overlap, chunk_size_limit=chunk_size)
+
+#define LLM 
+llmPredictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-ada-001", max_tokens=tokens))
+
+#load data
+docs = SimpleDirectoryReader(path).load_data()
+
+#create vector index
+vectorIndex = GTPSimpleVectorIndex(documents=docs, llm_predictor=llmPredictor, prompt_helper=prompt_helper)
+vectorIndex.save_to_disk('vectorIndex.json')
+return vectorIndex
+
+vectorIndex = createVectorIndex('knowledge')
