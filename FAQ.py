d1015efcb2b684bc056eaf41dbb1a4d2890fcce5
Håkon Lund-Isaksen <hakon.isaksen@aneo.com>
2023-06-12 15:08:24 +0200

    First commit

---
+++ FAQ.py
@@ -0,0 +1,7 @@
+from gtp_index import SimpleDirectoryReader, GTPListIndex, GTPSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import sys
+import os
+
+os.environ["OPEN_API_KEY"] = "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"
+
